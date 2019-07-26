f71d57cd3740c5e61f5438f778ca7f15fdce8c2e
89151 <891511545@qq.com>
2019-07-27 00:14:22 +0800

    mobi项目-管理平台

--- src/code/com/bubu/mobi/mapper/dao/MobiMenuMapper.java
+++ src/code/com/bubu/mobi/mapper/dao/MobiMenuMapper.java
@@ -1,8 +1,16 @@
 package code.com.bubu.mobi.mapper.dao;
 
 import code.com.bubu.mobi.bean.MobiMenu;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface MobiMenuMapper {
+
+    List<MobiMenu> selectMobiMenu();
+
+    int insertMobiMenu(@Param("name")String name,@Param("url")String url);
+
     /*int deleteByPrimaryKey(String id);
 
     int insert(MobiMenu record);
